3c94bf4bc81ed69d4c9d199cc0e30debddca4c2b
goutarouh <gouta.gorogoro@gmail.com>
2021-02-07 14:04:34 +0900

    簡単なエラー処理
    - 同一のcoroutineScope内であればtry-catch
    - キャンセルの場合cancellationExceptionを吐く

--- app/src/main/java/com/github/goutarouh/coroutin_study/MainActivity.kt
+++ app/src/main/java/com/github/goutarouh/coroutin_study/MainActivity.kt
@@ -2,6 +2,7 @@ package com.github.goutarouh.coroutin_study
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
@@ -10,35 +11,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         runBlocking {
-            val job1 = launch {
-                delay(100L)
-                "completed: job1".print()
+            val job = launch {
+                try {
+                    "start".print()
+                    delay(100L)
+                    "end".print()
+                } catch (e: CancellationException) {
+                    "catch: $e".print()
+                } finally {
+                    "finally".print()
+                }
             }
-            val job2 = launch {
-                delay(100L)
-                "completed: job2".print()
-            }
-            job1.join()
-            job2.join()
-            "completed: all".print()
-        }
-
-        runBlocking {
-            val job1 = launch {
-                "start: job1".print()
-                delay(100L)
-                "completed: job1".print()
-            }
-            val job2 = launch {
-                "start: job2".print()
-                delay(100L)
-                "completed: job2".print()
-            }
-
             delay(50L)
-            job1.cancel()
+            job.cancel()
         }
     }
 }
