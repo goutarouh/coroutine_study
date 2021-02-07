7301ee38f52bd2dfdc99d028de0c0264b6f61d09
goutarouh <gouta.gorogoro@gmail.com>
2021-02-07 20:26:09 +0900

    【Chapter6】6-1
    - Flowビルダー内でemitする
    - Coroutine内でcollectする

--- app/src/main/java/com/github/goutarouh/coroutin_study/MainActivity.kt
+++ app/src/main/java/com/github/goutarouh/coroutin_study/MainActivity.kt
@@ -3,31 +3,25 @@ package com.github.goutarouh.coroutin_study
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.coroutines.*
-import kotlin.coroutines.EmptyCoroutineContext
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        runBlocking {
-            val context = EmptyCoroutineContext
-            val scope = CoroutineScope(context)
-
-            //中段関数がない場合同期的に処理が進む可能性もある
-            scope.launch {
-                var i = 0
-                while (i <= 1_000_000) {
-                    if (i % 1_000 == 0) {
-                        i.print()
-                        yield()
-                    }
-                    i++
-                }
+        val flow = flow {
+            repeat(10) {
+                emit(it)
+                delay(1000L)
             }
-            //yieldを入れることで他の実行可能なcoroutineを探してくれる
-            scope.launch {
-                "hello".print()
+        }.map { it * 424 }
+
+        runBlocking {
+            flow.collect {
+                it.print()
             }
         }
     }
