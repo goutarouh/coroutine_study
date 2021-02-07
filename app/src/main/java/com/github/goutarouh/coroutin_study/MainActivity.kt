7152cb4f6262916677f63a2af279d26ae11cabbd
goutarouh <gouta.gorogoro@gmail.com>
2021-02-07 20:17:03 +0900

    【Chapter5】5-6
    - yieldで他の実行可能なcoroutineを探してくれる

--- app/src/main/java/com/github/goutarouh/coroutin_study/MainActivity.kt
+++ app/src/main/java/com/github/goutarouh/coroutin_study/MainActivity.kt
@@ -3,6 +3,7 @@ package com.github.goutarouh.coroutin_study
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.coroutines.*
+import kotlin.coroutines.EmptyCoroutineContext
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,49 +11,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         runBlocking {
-            val context1 = Job()
-            val scope1 = CoroutineScope(context1)
+            val context = EmptyCoroutineContext
+            val scope = CoroutineScope(context)
 
-            //5000で止まらない
-            scope1.launch {
+            //中段関数がない場合同期的に処理が進む可能性もある
+            scope.launch {
                 var i = 0
                 while (i <= 1_000_000) {
                     if (i % 1_000 == 0) {
                         i.print()
-                    }
-                    if (i == 500_000) {
-                        scope1.cancel()
+                        yield()
                     }
                     i++
                 }
             }
-
-            //isActiveで自身がキャンセルされていないか確認する
-            //(suspend関数は自信がキャンセルされているか確認する)
-            //5000で止まる
-            runBlocking {
-                val context2 = Job()
-                val scope2 = CoroutineScope(context2)
-                scope2.launch {
-                    var i = 0
-                    while (i <= 1_000_00 && isActive) {
-                        if (i % 1_000 == 0) {
-                            i.print()
-                        }
-                        if (i == 500_00) {
-                            scope2.cancel()
-                        }
-                        i++
-
-                        //キャンセルされていた場合
-                        //CancellationExceptionを出す
-                        //ensureActive()
-                    }
-                }
+            //yieldを入れることで他の実行可能なcoroutineを探してくれる
+            scope.launch {
+                "hello".print()
             }
         }
-
-
-
     }
 }
