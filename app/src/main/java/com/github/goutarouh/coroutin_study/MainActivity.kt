a85428cc8e33a7c7c6ecc2fa1ffad203c1df4e1d
goutarouh <gouta.gorogoro@gmail.com>
2021-02-07 13:45:11 +0900

    launchでCoroutineを開始する
    - Coroutinesを起動するにはCoroutineScopeが必要である。
    - runBlockingは新たなCoroutineScopeを起動し、その中で起動されたすべてのCoroutineの処理が完了するまで、処理をブロックする。

--- app/src/main/java/com/github/goutarouh/coroutin_study/MainActivity.kt
+++ app/src/main/java/com/github/goutarouh/coroutin_study/MainActivity.kt
@@ -2,10 +2,24 @@ package com.github.goutarouh.coroutin_study
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        runBlocking {
+            launch {
+                1.print()
+                delay(1000L)
+                2.print()
+            }
+            launch {
+                3.print()
+            }
+        }
     }
 }
