4c4789d6d208d3d7436e3f33610d330a51aa156d
goutarouh <gouta.gorogoro@gmail.com>
2021-02-07 13:51:27 +0900

    CoroutineContextについて
    - runBlockingは動作をブロックしてしまうため実コードでは使用頻度は少ない
    - CoroutinesContextはCoroutinesの挙動を定義する。
    - 一度キャンセルすると復活できない
    - コルーチンスコープをキャンセルするとそのスコープで起動されたすべてのコルーチンがキャンセルされる。

--- app/src/main/java/com/github/goutarouh/coroutin_study/MainActivity.kt
+++ app/src/main/java/com/github/goutarouh/coroutin_study/MainActivity.kt
@@ -11,15 +11,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        runBlocking {
-            launch {
-                1.print()
-                delay(1000L)
-                2.print()
-            }
-            launch {
-                3.print()
-            }
+        val scope = CoroutineScope(EmptyCoroutineContext)
+        scope.launch {
+            1.print()
+            delay(1000L)
+            2.print()
         }
+        scope.launch {
+            3.print()
+            delay(1000L)
+            4.print()
+        }
+        scope.cancel()
     }
 }
